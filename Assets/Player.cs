aa2ca32eaec3bffc6db4b511d3d89ea2fe200a5d
DESKTOP-TAFGOBQ\shev4 <s.v.shev4enk0@gmail.com>
2017-12-11 17:01:27 +0200

    rb.MovePosition

---
+++ Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10, jumpforse;
+
+    Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        var inputHorizontalVelosity = Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime;
+        print(inputHorizontalVelosity); 
+        var inputVerticalVelosity = Input.GetAxis("Vertical") * speed * Time.fixedDeltaTime;
+        var inputDirectionVelosity = new Vector3(inputHorizontalVelosity, inputVerticalVelosity, 0);
+        print(inputDirectionVelosity); 
+        rb.MovePosition(transform.position + inputDirectionVelosity);
+    }
+}
