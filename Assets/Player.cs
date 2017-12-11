cb519744945e8524ba4825fdcbda563024ec0d72
DESKTOP-TAFGOBQ\shev4 <s.v.shev4enk0@gmail.com>
2017-12-11 17:28:17 +0200

    Jump,IsGround

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,30 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 10, jumpforse;
+    public float speed = 10, jumpforse = 50;
+    public LayerMask layer;
 
     Rigidbody rb;
+
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+
     void FixedUpdate()
     {
-        Move();
+        if (IsGrounded())
+        {
+            Move();
+            Jump();
+        }
     }
 
     void Move()
     {
         var inputHorizontalVelosity = Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime;
-        print(inputHorizontalVelosity); 
         var inputVerticalVelosity = Input.GetAxis("Vertical") * speed * Time.fixedDeltaTime;
         var inputDirectionVelosity = new Vector3(inputHorizontalVelosity, inputVerticalVelosity, 0);
-        print(inputDirectionVelosity); 
         rb.MovePosition(transform.position + inputDirectionVelosity);
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up * jumpforse, ForceMode.Impulse);
+        }
+
+    }
+
+    bool IsGrounded()
+    {
+        var hit = Physics.Raycast(transform.position, Vector3.down, 0.5f, layer);
+        if (hit)
+        {
+            return true;
+        }
+            
+        return false;
     }
 }
