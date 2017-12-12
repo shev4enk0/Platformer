3efcae1d7305e2818d82d3ca08bf22d78da06964
DESKTOP-TAFGOBQ\shev4 <s.v.shev4enk0@gmail.com>
2017-12-12 22:45:22 +0200

    Try to use event

--- Assets/Egg.cs
+++ Assets/Egg.cs
@@ -5,11 +5,24 @@ using System;
 
 public class Egg : MonoBehaviour
 {
+    public event Action BrokenEgg;
+    public event Action CathedEgg;
+
     void OnCollisionEnter(Collision other)
     {
         if (other.collider.tag == "Ground")
-            Destroy(gameObject);
+        {
+            print("Ground"); 
+            if (BrokenEgg != null)
+                BrokenEgg();
+        }
+           
         if (other.collider.tag == "Hat")
-            Destroy(gameObject);
+        {
+            print("Hat"); 
+            if (CathedEgg != null)
+                CathedEgg();
+        }
+           
     }
 }
---
+++ Assets/ScoreCounter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour
+{
+
+    int score;
+
+    public  Egg egg;
+    Text scoreText;
+
+    void Start()
+    {
+//        egg = FindObjectOfType<Egg>();
+        egg.CathedEgg += Catched;
+
+        scoreText = GetComponent<Text>();
+        score = 0;
+    }
+
+    void Catched()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+    void Broken()
+    {
+    }
+}
